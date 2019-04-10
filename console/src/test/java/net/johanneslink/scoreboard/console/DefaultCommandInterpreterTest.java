bd8b147ddf9e422bdfbd2b6ed4837f315c434dfe
Anders, Julian <julian.anders@avl.com>
2019-04-10 12:08:56 +0200

    refactored defaulCommandInterpreterTest

--- console/src/test/java/net/johanneslink/scoreboard/console/DefaultCommandInterpreterTest.java
+++ console/src/test/java/net/johanneslink/scoreboard/console/DefaultCommandInterpreterTest.java
@@ -10,52 +10,47 @@ class DefaultCommandInterpreterTest {
 
     @Test
     void interpretQuit() {
-	assertEquals(Action.QUIT, interpreter.parse("quit"));
-	assertEquals(Action.QUIT, interpreter.parse("QUIT"));
-	assertEquals(Action.QUIT, interpreter.parse("q"));
-	assertEquals(Action.QUIT, interpreter.parse("Q"));
+	assertActionsInterpreted(Action.QUIT, "quit", "Quit", "q", "Q");
+    }
+
+    private void assertActionsInterpreted(final Action action, final String... inputs) {
+	for (final String input : inputs) {
+	    assertEquals(action, interpreter.parse(input));
+	}
     }
 
     @Test
     void interpretTeamASelection() {
-	assertEquals(Action.SELECT_A, interpreter.parse("a"));
-	assertEquals(Action.SELECT_A, interpreter.parse("select a"));
+	assertActionsInterpreted(Action.SELECT_A, "a", "select a");
     }
 
     @Test
     void interpretTeamBSelection() {
-	assertEquals(Action.SELECT_B, interpreter.parse("b"));
-	assertEquals(Action.SELECT_B, interpreter.parse("select b"));
+	assertActionsInterpreted(Action.SELECT_B, "b", "select b");
     }
 
     @Test
     void interpretScore1() {
-	assertEquals(Action.SCORE_1, interpreter.parse("1"));
-	assertEquals(Action.SCORE_1, interpreter.parse("score 1"));
+	assertActionsInterpreted(Action.SCORE_1, "1", "score 1");
     }
 
     @Test
     void interpretScore2() {
-	assertEquals(Action.SCORE_2, interpreter.parse("2"));
-	assertEquals(Action.SCORE_2, interpreter.parse("score 2"));
+	assertActionsInterpreted(Action.SCORE_2, "2", "score 2");
     }
 
     @Test
     void interpretScore3() {
-	assertEquals(Action.SCORE_3, interpreter.parse("3"));
-	assertEquals(Action.SCORE_3, interpreter.parse("score 3"));
+	assertActionsInterpreted(Action.SCORE_3, "3", "score 3");
     }
 
     @Test
     void interpretHelp() {
-	assertEquals(Action.HELP, interpreter.parse("?"));
-	assertEquals(Action.HELP, interpreter.parse("h"));
-	assertEquals(Action.HELP, interpreter.parse("help"));
+	assertActionsInterpreted(Action.HELP, "?", "h", "help");
     }
 
     @Test
     void interpretUnknownCommands() {
-	assertEquals(Action.UNKNOWN, interpreter.parse("c"));
-	assertEquals(Action.UNKNOWN, interpreter.parse("xxx"));
+	assertActionsInterpreted(Action.UNKNOWN, "c", "xxx");
     }
 }
